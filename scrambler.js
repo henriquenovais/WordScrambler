5e91de41c8a9b45ea6a1e43304ba7c60ab9f2603
Henrique Novais <henriquennoliveira@gmail.com>
2019-01-13 16:37:18 -0300

    Add commenting explaning code usage

--- scrambler.js
+++ scrambler.js
@@ -1,15 +1,18 @@
-// const sent = "Te desejo uma fé enorme, em qualquer coisa, não importa o quê, como aquela fé que a gente teve um dia, me deseja também uma coisa bem bonita, uma coisa qualquer maravilhosa, que me faça acreditar em tudo de novo, que nos faça acreditar em tudo outra vez.";
-let sent = "teste ,,,,teste,,,, teste";
+//This is the input sentence:
+let sentence = "Te desejo uma fé enorme, em qualquer coisa, não importa o quê, como aquela fé que a gente teve um dia, me deseja também uma coisa bem bonita, uma coisa qualquer maravilhosa, que me faça acreditar em tudo de novo, que nos faça acreditar em tudo outra vez.";
+//Comma counter:
 let commas = 0;
+//Dot counter:
 let dots = 0;
-
-for(i = 0;i < sent.length;i++){
+//Loop for comma and dott detection in order to leave the words ready for comparison:
+for(i = 0;i < sentence.length;i++){
  
-    if(sent[i] === ","){
+    if(sentence[i] === ","){
 
         commas = commas + 1;
         console.log(commas);
-    } else if(sent[i] === "."){
+
+    } else if(sentence[i] === "."){
 
        dots = dots + 1;
        console.log(dots);
@@ -17,19 +20,27 @@ for(i = 0;i < sent.length;i++){
     }
 }
 
+//Removing commas according to the variable comma's value:
 for(i = 0;i < commas;i++){
 
-    sent = sent.replace(",","");
+    sentence = sentence.replace(",","");
     
 }
 
+//Removing dotts according to the variable dott's value:
 for(i = 0;i < dots;i++){
 
-    sent = sent.replace(".","");
+    sentence = sentence.replace(".","");
 
 }
+//Now the sentence should be parsed accordingly(no commas and dots),
+//Splitting the parsed sentence using a space(" ") as the splitting factor.
+/*
+For example:
+    "I love cats" -> "I","love","cats"
+*/ 
+let sentence_words = sentence.split(" ");
 
 
-let sent_words = sent.split(" ");
-console.log(sent_words);
+console.log(sentence_words);
 
